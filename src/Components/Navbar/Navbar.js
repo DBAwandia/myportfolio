56ae387a81435fc66da983700a567100f5cc337e
ken <kmaina510@gmail.com>
2022-12-01 17:53:34 +0300

    added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,61 @@
 import React, { useState } from 'react'
 import "./Navbar.css"
 import { HashLink as Link } from 'react-router-hash-link';
+import { AutoFixHighTwoTone, Dehaze, FormatAlignLeft, Laptop, Person, Public, Swipe } from '@mui/icons-material';
 
 function Navbar() {
   const [active , setActive ] = useState("")
   
   return (
     <div className='navbar'>
+      <div className='sidebar'>
+          <FormatAlignLeft className="swipe"/>
+      </div>
+          <div className='sidebar_close'>
+          
+          </div>
+          <div className='sidebar_icons'>
+          <Link to="#skills" smooth>
+            <div className='icon_skillz'>
+              <Laptop className='laptop'/>
+              <p className={active === "Skills"? "active_Link": "normal_link"}
+                onClick={()=>{
+                  setActive("Skills")
+                }}
+              >Skills</p>
+            </div>
+          </Link>
+          <Link to="#projects" smooth>
+            <div className='icon_skillz'>
+              <AutoFixHighTwoTone className='projects'/>
+            <p className={active ==="Projects"? "active_Link": "normal_link"}
+              onClick={()=>{
+                setActive("Projects")
+              }}
+            >Projects</p>
+            </div>
+          </Link>
+          <Link to="#personal" smooth>
+          <div className='icon_skillz'>
+            <Person className='person' />
+            <p className={active === "personal"? "active_Link": "normal_link"}
+                onClick={()=>{
+                  setActive("personal")
+                }}
+              >About me</p>
+          </div>
+          </Link>
+          <Link to="#mysocials" smooth>
+            <div className='icon_skillzz'>
+              <Public className='public' />
+              <p className={active ==="About"? "active_Link": "normal_link"}
+                onClick={()=>{
+                  setActive("About")
+                }}
+              >My socials</p>
+            </div>
+          </Link>
+          </div>
       <div className='nav_container'>
         <div className='logo'>
           Home
--- src/Components/Pages/Skills/Skills.js
+++ src/Components/Pages/Skills/Skills.js
@@ -1,33 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./Skills.css"
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
-
 function Skills() {
   const skillsData = [
     {
       id: 1,
-      image: "/images/html.png",
-      title: "HTML",
-      description: "I use this language for creating Websites."
-    },
-    {
-      id: 2,
-      image: "/images/css.png",
-      title: "CSS",
-      description: "I use this language for styling and designing Websites."
-    },
-    {
-      id: 3,
       image: "/images/javascript.png",
       title: "Javascript",
       description: "I use this programming language for making websites frontend and backend functionalities ."
     },
     {
-      id: 4,
+      id: 2,
       image: "/images/react.png",
       title: "React JS || React Native",
       description: "I use this tools for making webapps."
+    },
+    {
+      id: 3,
+      image: "/images/html.png",
+      title: "HTML",
+      description: "I use this language for creating Websites."
+    },
+    {
+      id: 4,
+      image: "/images/css.png",
+      title: "CSS",
+      description: "I use this language for styling and designing Websites."
     },
     {
       id: 5,
@@ -36,20 +34,23 @@ function Skills() {
       description: "I am very experienced in MongoDB as database."
     }
 ]
+
   return (
+    
+
     <div className='skills' id='skills'>
-      <div className='skills_container'>
-        <div className='skills_header'>
-          <h1>My </h1><span>Skills</span>
+       
+
+       <div className='skills_container'>
+       
+       
+       <div className='skills_header'>
+          <h1>My</h1><span>Skills</span>
         </div>
-          <TrackVisibility>
-       {({isVisible}) =>  
-       <div className={isVisible? "animate__fadeInLeft":"skills_desc"}>
-          <p>Tools and programming languages that I use for development and making projects</p>
-        </div>
-        }
-        </TrackVisibility>
-        
+                <div className="skills_desc">
+                  <p>Tools and programming languages that I use for development and making projects</p>
+                 </div>
+
         <div className='skills_languages'>
         {skillsData.map((item) =>{
           return <div className='skillz_container' key={item.id}>
@@ -67,8 +68,12 @@ function Skills() {
           </div>
           })}
         </div>
-      </div>
+
+      </div> 
+   
     </div>
+    
+
   )
 }
 
