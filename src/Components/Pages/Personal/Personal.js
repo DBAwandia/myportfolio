b92fa9b67cac34cb80b79e27de183cdfa7a77ee3
ken <kmaina510@gmail.com>
2022-12-08 16:07:27 +0300

    added

--- src/Components/Pages/Personal/Personal.js
+++ src/Components/Pages/Personal/Personal.js
@@ -6,15 +6,16 @@ let educations = "https://media.tenor.com/qJ5evVs-_uUAAAAC/coding.gif"
   return (
     <div className='personal' id='personal'>
       <div className='personal_education'>
+            <div className='education_header'>
+                My
+                background
+            </div>
         <div className='education_gif'>
             <img src={educations} alt="" />
         </div>
         <div className='education_desc'>
-            <div className='education_header'>
-                <b>My</b>
-                <span>background</span>
-            </div>
-            <p>Hi, Everyone I am <b style={{color:"blue"}}>Computer science</b>  student, currently I am studying in University of Eldoret,Kenya .
+            
+            <p>Hi, Everyone I am <b style={{color:"gray"}}>Computer science</b>  student, currently I am studying in University of Eldoret,Kenya .
                 I am a passionate Full stack Web Developer. 
                 I am always ready to have new experiences and learn new technologies and implement them in my life.
                 I learn new things and help people by creating impact through my developing skills and by solving their problem.
